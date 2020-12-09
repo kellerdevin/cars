8b27f36d5481181f3eadf8b26173181ea7943d53
Devin Keller <dkeller@chapman.edu>
2020-12-08 18:35:34 -0800

    Fixed  cars.sh

--- cars.sh
+++ cars.sh
@@ -5,15 +5,15 @@
 while true;
 do
 	echo "Press 1 to enter a new car, 2 to display the list of cars, and 3 to quit."
-	read userchoice
+	read -r userchoice
 	case $userchoice in
 	1)
 		echo "Please enter the year the car was made: " 
-		read year
+		read -r year
 		echo "Please enter the make of the car: "
-		read make
+		read -r make
 		echo "Please enter the model of the car: "
-		read model
+		read -r model
 		cars="$year:$make:$model"
 		echo "$cars" >> "My_old_cars"
 		echo "Your car has been successfully added to the list."
@@ -32,4 +32,4 @@ do
 	esac
 done
 
-echo "Successfully quit." 
+echo "Successfully quit."
